23d1c3525840291468825137fd2f7f0a26c5c0ba
hrgweb <gamayonhergenroque.web@gmail.com>
2021-02-15 15:36:24 +0800

    register vuetify and add main app

---
+++ resources/js/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-icon>mdi-access-point</v-icon>
+
+        <v-icon large color="green darken-2"> mdi-domain </v-icon>
+
+        <v-icon large color="blue darken-2"> mdi-message-text </v-icon>
+
+        <v-icon large color="purple darken-2"> mdi-dialpad </v-icon>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Vue from "vue";
+import vuetify from "./vuetify";
+
+import App from "./App.vue";
+
+new Vue({
+    el: "#app",
+    vuetify,
+    render: (h) => h(App),
+});
