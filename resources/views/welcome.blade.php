b79419e80dc9b0289f3cfd797e109176c7d7123d
hrgweb <gamayonhergenroque.web@gmail.com>
2021-02-15 17:39:33 +0800

    setup the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Elite Anywhere - Exam</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
-  </head>
-<body>
-    <div id="app">
-    </div>
+@extends('layouts.app')
 
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@section('content')
+    <app>
+        <h1>Welcome Page</h1>
+    </app>
+@endsection
